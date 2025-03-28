c061fa7d3ce92829b55b0c0daacb8cb94a59f615
Mateo Randulfe <mateorandulfe@gmail.com>
2025-03-28 17:16:02 +0000

    feat: api layer

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+// .eslintrc.cjs
+
+module.exports = {
+  root: true,
+  ignorePatterns: ['**/dist/**', '**/node_modules/**'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+}
